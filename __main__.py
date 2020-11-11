eaecd3dda7be71f406ff8977fc0d118901dc8591
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-11-10 22:36:31 -0500

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         folder_path = sys.argv[1]
     else:
-        folder_path = r"C:\Users\gince\Documents\Laval_University\cours_A20\Apprentissage_par_renforcement_IFT-4201\Contenue\Semaine 5 - Les approches Bayésiennes"
+        folder_path = r"C:\Users\gince\Documents\Laval_University\cours_A20\Apprentissage_par_renforcement_IFT-4201\Contenue\Semaine 10 - Algorithmes d'apprentissage par renforcement séquentiel"
 
     if len(sys.argv) > 2:
         language = sys.argv[2]
@@ -14,7 +14,7 @@ if __name__ == '__main__':
         language = "fr-CA"
 
     VideoReader(
-        take_speech=True,
+        take_speech=False,
         verbose=False,
         language=language,
     ).make_pdf_from_folder(folder_path)
