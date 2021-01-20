ffa931e8fdfb50857496efb628b4a7489028fcef
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2021-01-20 10:29:02 -0500

    Update pdf_merge.py

--- modules/pdf_merge.py
+++ modules/pdf_merge.py
@@ -1,10 +1,27 @@
 import os
 from PyPDF2 import PdfFileMerger
+import re
+
+
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+
+def natural_keys(text):
+    '''
+    alist.sort(key=natural_keys) sorts in human order
+    http://nedbatchelder.com/blog/200712/human_sorting.html
+    (See Toothy's implementation in the comments)
+    '''
+    return [atoi(c) for c in re.split(r'(\d+)', text)]
+
 
 path: str = r"D:\Documents\Laval_University\cours_H21\DeepLearning\Book"
 
 x = [a for a in os.listdir(path) if a.endswith(".pdf")]
-x.sort()
+print(x)
+x.sort(key=natural_keys)
+print(x)
 
 merger = PdfFileMerger()
 
